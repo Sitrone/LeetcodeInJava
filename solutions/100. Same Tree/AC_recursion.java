4a71b1eb1e6e2ab19263447a168a245c53c5d712
sitrone <sununiq@163.com>
2016-06-26 17:16:52 +0800

    add annotation

--- solutions/100. Same Tree/AC_recursion.java
+++ solutions/100. Same Tree/AC_recursion.java
@@ -1,6 +1,6 @@
 public class SameTree {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null)
+        if (p == null && q == null)                        //终止条件
             return true;
         if (p == null || q == null)
             return false;
