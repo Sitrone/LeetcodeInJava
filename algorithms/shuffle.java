dee4a89d7c7faffa72f2474ce15d8126ee6ab62e
sitrone <sununiq@163.com>
2016-07-23 18:51:21 +0800

    洗牌程序，Fisher–Yates算法

---
+++ algorithms/shuffle.java
@@ -0,0 +1,20 @@
+package com.algs;
+
+import java.util.Random;
+
+public class Shuffle {
+	public static void shuffle(int[] nums) {
+		// Fisher–Yates shuffle
+		// 每次从剩余的数组中随机取出一个数与最后一个数交换
+		// 默认rand是安全的
+		Random rd = new Random();
+		int len = nums.length;
+		int j = len - 1;
+		while (j-- > 1) {
+			int index = rd.nextInt(j + 1);
+			int a = nums[index];
+			nums[index] = nums[j];
+			nums[j] = a;
+		}
+	}
+}
