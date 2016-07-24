8e34f23f904d5e010021cdc9d010e5108f6d85e5
sitrone <sununiq@163.com>
2016-07-24 18:50:19 +0800

    摩尔投票法求数组中出现超过一半的数字，与发帖水王那个题一样

---
+++ solutions/169. Majority Element/AC_MoorVote.java
@@ -0,0 +1,24 @@
+﻿	/**
+	 * 1、Moore voting algorithm,摩尔投票法
+	 * wiki：https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm
+	 *
+	 */
+public class MajorityElement {
+    public static int majorityElement(int[] nums) {
+    	// 
+        int times = 0;
+        int majority = 0;
+        for(int i = 0; i < nums.length; i++){
+        	// 如果出现次数为0，则前面没有出现大于一般的数字，重新设置新的数字
+        	if(times == 0){
+        		majority = nums[i];
+        	}
+        	if(nums[i] != majority){
+        		times++;
+        	}else{
+        		times--;
+        	}
+        }
+        return majority;
+    }
+}
