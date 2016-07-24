93a3fa60dcda053f3b487451bbdf9a24400ecb91
sitrone <sununiq@163.com>
2016-07-24 12:54:51 +0800

    bit运算模拟加法，

---
+++ solutions/371. Sum of Two Integers/AC_bit manipulation_iteration.java
@@ -0,0 +1,18 @@
+﻿	/**
+	 * 1、异或操作求的结果
+	 * 2、与操作模拟进位
+	 *
+	 */
+public class Solution {
+    public int getSum(int a, int b) {
+		int result, carry;
+        do{
+			result = a ^ b; 
+			carry = (a & b) << 1;
+			a = result;
+			b = carry;
+		}while(carry != 0);
+		
+        return result;
+    }
+}
---
+++ solutions/371. Sum of Two Integers/AC_bit manipulation_recursive.java
@@ -0,0 +1,15 @@
+﻿	/**
+	 * 1、异或操作求的结果
+	 * 2、与操作模拟进位
+	 *
+	 */
+public class Solution {
+    public int getSum(int a, int b) {
+        int result = a ^ b; 
+        int carry = (a & b) << 1;
+        if (carry != 0) {
+            return getSum(result, carry);
+        }
+        return result;
+    }
+}
