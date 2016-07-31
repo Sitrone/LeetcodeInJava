893418605a50eada0f1147916fd780cb595d5e56
sitrone <sununiq@163.com>
2016-07-31 20:23:48 +0800

    查找数组中的重复数字，先排序或者使用HashSet

---
+++ solutions/217. Contains Duplicate/AC_HashSet.java
@@ -0,0 +1,18 @@
+﻿	/**
+	 * 使用HashSet
+	 * 
+	 *
+	 */
+public class ContainsDuplicate {
+	//HashSet
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(int i = 0; i < nums.length; i++){
+        	if(set.contains(nums[i])){
+        		return true;
+        	}
+        	set.add(nums[i]);
+        }
+        return false;
+    }
+}
---
+++ solutions/217. Contains Duplicate/AC_sort.java
@@ -0,0 +1,15 @@
+﻿	/**
+	 * 先排序，再比较相邻的数
+	 * 
+	 *
+	 */
+public class ContainsDuplicate {
+	// Sort
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; ++i) {
+            if (nums[i] == nums[i + 1]) return true;
+        }
+        return false;
+    }
+}
