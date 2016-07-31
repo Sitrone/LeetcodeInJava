3ff0945ed085c83f12a02fd3b13b706f8d16f3b6
sitrone <sununiq@163.com>
2016-07-31 19:35:41 +0800

    奇偶链表

---
+++ solutions/328. Odd Even Linked List/AC_draw.java
@@ -0,0 +1,23 @@
+﻿	/**
+	 * 
+	 * 
+	 *
+	 */
+public class OddEvenList {
+	public ListNode oddEvenList(ListNode head){
+		if(head == null) return null;
+		
+		ListNode p1 = head;
+		ListNode p2 = head.next;
+		ListNode connectNode = p2;
+		while(p2 != null && p2.next != null){
+			p1.next = p2.next;
+			p1 = p1.next;
+					
+			p2.next = p1.next;
+			p2 = p2.next;
+		}
+		p1.next = connectNode;
+		return head;
+	}
+}
