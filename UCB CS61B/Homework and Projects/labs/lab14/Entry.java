5d1557d5bc4be16008799ef2c059527cd142138c
sitrone <sununiq@163.com>
2016-12-17 21:40:07 +0800

    finish lab14

---
+++ UCB CS61B/Homework and Projects/labs/lab14/Entry.java
@@ -0,0 +1,34 @@
+/* Entry.java */
+
+package com.cs61b.lab14;
+
+/**
+ * A class for dictionary entries, which are (key, value) pairs.
+ *
+ * DO NOT CHANGE THIS FILE. It is part of the interface of the Dictionary ADT.
+ **/
+
+public class Entry
+{
+
+    protected Object key;
+    protected Object value;
+
+    protected Entry(){}
+
+    protected Entry(Object k, Object v)
+    {
+        key = k;
+        value = v;
+    }
+
+    public Object key()
+    {
+        return key;
+    }
+
+    public Object value()
+    {
+        return value;
+    }
+}
