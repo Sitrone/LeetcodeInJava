3eca73bb70b1258993c1810d9d0014f7e89b8abb
sitrone <sununiq@163.com>
2016-12-17 20:38:49 +0800

    update disjoint set

--- UCB CS61B/Homework and Projects/homework/hw9/set/DisjointSets.java
+++ UCB CS61B/Homework and Projects/homework/hw9/set/DisjointSets.java
@@ -1,6 +1,6 @@
 /* DisjointSets.java */
 
-package set;
+package com.cs61b.hw9.set;
 
 /**
  *  A disjoint sets ADT.  Performs union-by-size and path compression.
@@ -14,7 +14,8 @@ package set;
  *  Elements are represented by ints, numbered from zero.
  **/
 
-public class DisjointSets {
+public class DisjointSets 
+{
 
   private int[] array;
 
@@ -24,9 +25,11 @@ public class DisjointSets {
    *  @param numElements the initial number of elements--also the initial
    *  number of disjoint sets, since every element is initially in its own set.
    **/
-  public DisjointSets(int numElements) {
+  public DisjointSets(int numElements) 
+  {
     array = new int [numElements];
-    for (int i = 0; i < array.length; i++) {
+    for (int i = 0; i < array.length; i++) 
+    {
       array[i] = -1;
     }
   }
@@ -40,15 +43,23 @@ public class DisjointSets {
    *  @param root1 the root of the first set.
    *  @param root2 the root of the other set.
    **/
-  public void union(int root1, int root2) {
-    if (array[root2] < array[root1]) {                 // root2 has larger tree
-      array[root2] += array[root1];        // update # of items in root2's tree
-      array[root1] = root2;                              // make root2 new root
-    } else {                                  // root1 has equal or larger tree
-      array[root1] += array[root2];        // update # of items in root1's tree
-      array[root2] = root1;                              // make root1 new root
+    public void union(int root1, int root2)
+    {
+        if (array[root1] >= 0 || array[root2] >= 0 || root1 == root2)
+        {
+            throw new IllegalArgumentException("Error: not the root");
+        }
+        if (array[root2] < array[root1])  // root2 has larger tree
+        { 
+            array[root2] += array[root1]; // update # of items in root2's tree
+            array[root1] = root2;         // make root2 new root
+        }
+        else
+        {                                  // root1 has equal or larger tree
+            array[root1] += array[root2]; // update # of items in root1's tree
+            array[root2] = root1;         // make root1 new root
+        }
     }
-  }
 
   /**
    *  find() finds the (int) name of the set containing a given element.
@@ -57,15 +68,25 @@ public class DisjointSets {
    *  @param x the element sought.
    *  @return the set containing x.
    **/
-  public int find(int x) {
-    if (array[x] < 0) {
-      return x;                         // x is the root of the tree; return it
-    } else {
-      // Find out who the root is; compress path by making the root x's parent.
-      array[x] = find(array[x]);
-      return array[x];                                       // Return the root
+    public int find(int x)
+    {
+        if (array[x] < 0)
+        {
+            return x; // x is the root of the tree; return it
+        }
+        else
+        {
+            // Find out who the root is; compress path by making the root x's parent.
+            array[x] = find(array[x]);
+            return array[x]; // Return the root
+        }
     }
-  }
+    
+    
+    public boolean isSameSet(int x, int y)
+    {
+        return find(x) == find(y);
+    }
 
   /**
    *  main() is test code.  All the find()s on the same output line should be
