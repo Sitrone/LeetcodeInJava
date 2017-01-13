e5b8ecce8cf02b782bd7005f212805da79c27d2b
sitrone <sununiq@163.com>
2017-01-13 22:30:04 +0800

    fix problems

--- Algorithms 4/Part Ⅰ/week 3/hw/FastCollinearPoints.java
+++ Algorithms 4/Part Ⅰ/week 3/hw/FastCollinearPoints.java
@@ -1,108 +1,80 @@
 package com.algs4.hw3;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 public class FastCollinearPoints
 {
-	private Map<Double, List<Point>> foundSegs = new HashMap<>();
-	private List<LineSegment> segments = new LinkedList<>();
-	
-	// finds all line segments containing 4 points
-	public FastCollinearPoints(Point[] points)
-	{
-		if(points == null)
-			throw new NullPointerException();
-		if(points.length < 3) 
-			return;
-		if(hasDuplicate(points))
+    private HashMap<Double, List<Point>> foundSegments = new HashMap<>();
+    private List<LineSegment> segments = new ArrayList<>();
+
+
+    public FastCollinearPoints(Point[] points) {
+		if (hasDuplicate(points))
 			throw new IllegalArgumentException();
-		Point[] pointsCopy = Arrays.copyOf(points, points.length);
 		
-		for (Point point : pointsCopy)
-		{
-			Arrays.sort(pointsCopy, point.slopeOrder());
-			
-			List<Point> slopePoints = new LinkedList<>();
+		Point[] pointsCopy = Arrays.copyOf(points, points.length);
+        for (Point startPoint : points) {
+            Arrays.sort(pointsCopy, startPoint.slopeOrder());
+
+            List<Point> slopePoints = new ArrayList<>();
             double slope = 0;
             double previousSlope = Double.NEGATIVE_INFINITY;
-			for(int i = 1; i < pointsCopy.length; i++)
-			{
-				slope = point.slopeTo(pointsCopy[i]);
-				if(previousSlope == slope)
-				{
-					slopePoints.add(pointsCopy[i]);
-				}
-				else
-				{
-					if(slopePoints.size() > 3)
-					{
-						slopePoints.add(point);
-						addSegment(slope, slopePoints);
-					}
+
+            for (int i = 1; i < pointsCopy.length; i++) {
+                slope = startPoint.slopeTo(pointsCopy[i]);
+                if (slope == previousSlope) {
+                    slopePoints.add(pointsCopy[i]);
+                } else {
+                    if (slopePoints.size() >= 3) {
+                        slopePoints.add(startPoint);
+                        addSegmentIfNew(slopePoints, previousSlope);
+                    }
                     slopePoints.clear();
                     slopePoints.add(pointsCopy[i]);
-				}
-				previousSlope = slope;
-			}
-			if(slopePoints.size() > 3)
-			{
-				slopePoints.add(point);
-				addSegment(slope, slopePoints);
-			}
-		}
-	}
-	
-	private void addSegment(Double slope, List<Point> slopePoints)
-	{
-        List<Point> endPoints = foundSegs.get(slope);
+                }
+                previousSlope = slope;
+            }
+
+            if (slopePoints.size() >= 3) {
+                slopePoints.add(startPoint);
+                addSegmentIfNew(slopePoints, slope);
+            }
+        }
+    }
+
+    private void addSegmentIfNew(List<Point> slopePoints, double slope) {
+        List<Point> endPoints = foundSegments.get(slope);
         Collections.sort(slopePoints);
 
         Point startPoint = slopePoints.get(0);
         Point endPoint = slopePoints.get(slopePoints.size() - 1);
 
-		if (endPoints == null)
-		{
-			endPoints = new LinkedList<>();
-			endPoints.add(endPoint);
-			foundSegs.put(slope, endPoints);
-			segments.add(new LineSegment(startPoint, endPoint));
-		}
-		else
-		{
-			for (Point currentEndPoint : endPoints)
-			{
-				if (currentEndPoint.compareTo(endPoint) == 0)
-				{
-					return;
-				}
-			}
-			endPoints.add(endPoint);
-			segments.add(new LineSegment(startPoint, endPoint));
-		}
-	}
+        if (endPoints == null) {
+            endPoints = new ArrayList<>();
+            endPoints.add(endPoint);
+            foundSegments.put(slope, endPoints);
+            segments.add(new LineSegment(startPoint, endPoint));
+        } else {
+            for (Point currentEndPoint : endPoints) {
+                if (currentEndPoint.compareTo(endPoint) == 0) {
+                    return;
+                }
+            }
+            endPoints.add(endPoint);
+            segments.add(new LineSegment(startPoint, endPoint));
+        }
+    }
 
-	// the number of line segments
-	public int numberOfSegments() 
-	{
-		return segments.size();
-	}
 
-	// the line segments
-	public LineSegment[] segments() 
-	{
-		return segments.toArray(new LineSegment[0]);
-	}
-	
 	private boolean hasDuplicate(Point[] points)
 	{
-		for(int i = 0; i < points.length - 1; i++)
+		for (int i = 0; i < points.length - 1; i++)
 		{
-			if(points[i].equals(points[i + 1]))
+			if (points[i].equals(points[i + 1]))
 			{
 				return true;
 			}
@@ -110,4 +82,11 @@ public class FastCollinearPoints
 		return false;
 	}
 
+    public int numberOfSegments() {
+        return segments.size();
+    }
+
+    public LineSegment[] segments() {
+        return segments.toArray(new LineSegment[segments.size()]);
+    }
 }
