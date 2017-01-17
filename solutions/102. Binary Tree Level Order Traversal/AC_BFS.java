a0f7946d25adafc2ebccf0edac80be1aa52db78d
sitrone <sununiq@163.com>
2017-01-17 20:55:55 +0800

    b tree level order

---
+++ solutions/102. Binary Tree Level Order Traversal/AC_BFS.java
@@ -0,0 +1,29 @@
+﻿	/**
+	 * 
+	 * 
+	 *
+	 */
+public class LevelOrder
+{
+    public List<List<Integer>> levelOrder(TreeNode root) {
+    	List<List<Integer>> res = new ArrayList<List<Integer>>();
+    	if(root == null) return res;
+    	
+    	Queue<TreeNode> q = new LinkedList<>();
+    	q.offer(root);
+    	
+    	while(!q.isEmpty())
+    	{
+    		List<Integer> level = new ArrayList<>();
+    		int num = q.size();
+    		for(int i = 0; i < num; i++)
+    		{
+    			TreeNode cur = q.poll();
+    			level.add(cur.val);
+        		if(cur.left != null) q.offer(cur.left);
+        		if(cur.right != null) q.offer(cur.right);
+    		}
+    		res.add(level);
+    	}
+    	return res;
+    }
